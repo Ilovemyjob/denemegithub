4f900b5fd90ffe8d8acbca00e5be8d83cdc6ac03
emrahalp <64224084+Ilovemyjob@users.noreply.github.com>
2021-01-19 21:51:31 +0100

    Update deneme.java

--- src/test/java/denemegithub/deneme.java
+++ src/test/java/denemegithub/deneme.java
@@ -4,6 +4,7 @@ public class deneme {
     public static void main(String[] args) {
         System.out.println("first");
         System.out.println("second");
+        System.out.println("ucuncu");
     }
 }
 
